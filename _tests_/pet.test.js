b975374979c833e02d3eeeca214101c5d98a6983
Hande Gungor <handegungor12@gmail.com>
2022-03-17 21:07:45 +0000

    coming alive

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -3,4 +3,8 @@ describe('constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Aslan')).toBeInstanceOf(Object);
     });
+        it('sets the name property', () => {
+            const pet = new Pet('Aslan');
+            expect(pet.name).toEqual('Aslan');
+    });
 });
