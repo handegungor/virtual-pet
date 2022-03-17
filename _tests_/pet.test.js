0bccc371ee94bfe1936c3de3887eade3c50ae018
Hande Gungor <handegungor12@gmail.com>
2022-03-17 21:21:40 +0000

    growing up

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -7,4 +7,15 @@ describe('constructor', () => {
             const pet = new Pet('Aslan');
             expect(pet.name).toEqual('Aslan');
     });
+        it('has an initial age of 0', () => {
+            const pet = new Pet('Fido');
+            expect(pet.age).toEqual(0);
+    });
+    describe('growUp', () => {
+        it('increments the age by 1', () => {
+            const pet = new Pet('Aslan');
+            pet.growUp();
+            expect(pet.age).toEqual(1);
+        });
+        });
 });
