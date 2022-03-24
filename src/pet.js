641fd529c60201fdd1188b67f6e752e84e2a7e4b
Hande Gungor <handegungor12@gmail.com>
2022-03-24 20:46:40 +0000

    virtual pet

--- src/pet.js
+++ src/pet.js
@@ -4,5 +4,13 @@ function Pet(name) {
 }
 Pet.prototype.growUp = function() {
     this.age += 1;
+    this.hunger += 5;
     };
+ Pet.prototype.walk = function() {
+    if ((this.fitness + 4) <= 10) {
+        this.fitness += 4;
+    } else {
+        this.fitness = 10;
+    }
+    }
 module.exports = Pet;
