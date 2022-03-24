0d17a117440af8a3e56992bce2e9128a839e9955
Hande Gungor <handegungor12@gmail.com>
2022-03-24 20:46:04 +0000

    virtual pet

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -1,11 +1,11 @@
 const Pet = require('../src/pet');
 describe('constructor', () => {
     it('returns an object', () => {
-        expect(new Pet('Aslan')).toBeInstanceOf(Object);
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
         it('sets the name property', () => {
-            const pet = new Pet('Aslan');
-            expect(pet.name).toEqual('Aslan');
+            const pet = new Pet('Fido');
+            expect(pet.name).toEqual('Fido');
     });
         it('has an initial age of 0', () => {
             const pet = new Pet('Fido');
@@ -13,9 +13,17 @@ describe('constructor', () => {
     });
     describe('growUp', () => {
         it('increments the age by 1', () => {
-            const pet = new Pet('Aslan');
+            const pet = new Pet('Fido');
             pet.growUp();
             expect(pet.age).toEqual(1);
         });
-        });
+    });
+describe('walk', () => {
+    it('increases fitness by to a maximum of 10', () => {
+        const pet = new Pet('fido');
+        pet.fitness = 8;
+        pet.walk();
+        expect(pet.fitness).toEqual(10);
+    });
+});
 });
